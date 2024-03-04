9cc7d54c6a27003ad3fdd870972acf2c97ed94d0
dealofalifes <korayerdn@gmail.com>
2024-03-04 15:52:12 +0700

    removee

--- MuseumTycoon/Assets/Script/Managers/EditObjsManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EditObjsManager : MonoBehaviour
-{
-    List<EditObjBehaviour> Statues = new List<EditObjBehaviour>();
-    List<EditObjBehaviour> Decorations = new List<EditObjBehaviour>();
-}
--- MuseumTycoon/Assets/Script/UI/PanelClickHandler.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-
-public class PanelClickHandler : MonoBehaviour
-{
-    //Bu Scriptin tek amacý RoomData'da RoofLock Canvasýna ulaþmaktýr. Lütfen silmeyiniz.
-}
