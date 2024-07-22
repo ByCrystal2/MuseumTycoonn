34e0ba366faf89b4a55cfbb87ed46e91c4363031
Ahmet Burak <Ahmet_mustafa_0404@hotmail.com>
2024-07-22 22:12:54 +0300

    WayDataConnections Updated.

---
+++ Assets/Script/Editorrr/AutoRoomWallActiveCalculate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRoomWallActiveCalculate : MonoBehaviour
+{
+    private void Awake()
+    {
+//#if UNITY_EDITOR
+//        gameObject.SetActive(true);
+//#else
+
+
+    }
+   
+}
