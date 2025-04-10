71786d498e096caa744d5d702510c6658663fe0f
Fatma Gül Gök <148465338+fatmagulgk@users.noreply.github.com>
2025-04-10 17:00:12 +0300

    Google play games configuration changed

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = "230815727950"; // Filled in automatically
+        public const string ApplicationId = "1095852848857"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "230815727950-6lr9ac9kndphgebfp30ogonejkqiqa4a.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = "1095852848857-bhram178hciepat02j2djllb4bnbu5mv.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
